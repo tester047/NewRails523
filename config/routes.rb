d300780b6b1d18dc911298a60506a95b328636d0
tester047 <mailbox@ruby.com>
2019-11-04 21:49:56 +0300

    Routes setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  namespace :users do
-    get 'omniauth_callbacks/facebook'
-  end
-  devise_for :users
-  root to: 'home#index'
+  # namespace :users do
+  #   get 'omniauth_callbacks/facebook'
+  # end
+  # devise_for :users
 
   resources :users
+  root to: 'users#index'
 end
